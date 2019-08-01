d98d80894cbfdd56d3c1d2626ac1bfddc53ddfc4
HelodieJaqueline <1037102215@qq.com>
2019-08-01 21:45:50 +0800

    MaxProfit

--- zhangrui-api/src/main/java/com/zhangrui/algorithms/string/TitleToNumber.java
+++ zhangrui-api/src/main/java/com/zhangrui/algorithms/string/TitleToNumber.java
@@ -1,10 +1,30 @@
 package com.zhangrui.algorithms.string;
 
 /**
- * @Author: ZhangRui
- * @Date: Created at 2019-07-31-16:13
- * @Description:
- * @Modified: By
+ * Given a column title as appear in an Excel sheet, return its corresponding column number.
+ * For example:
+ *     A -> 1
+ *     B -> 2
+ *     C -> 3
+ *     ...
+ *     Z -> 26
+ *     AA -> 27
+ *     AB -> 28
+ *     ...
+ * Example 1:
+ *
+ * Input: "A"
+ * Output: 1
+ *
+ * Example 2:
+ *
+ * Input: "AB"
+ * Output: 28
+ *
+ * Example 3:
+ *
+ * Input: "ZY"
+ * Output: 701
  */
 public class TitleToNumber {
 
