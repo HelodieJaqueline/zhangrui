a91efa8852c8a1af851f6f70bd9d705d9cbb2970
HelodieJaqueline <1037102215@qq.com>
2019-10-10 21:52:56 +0800

    AQS

---
+++ zhangrui-api/src/main/java/com/zhangrui/concurrency/ReentrantDemo.java
@@ -0,0 +1,17 @@
+package com.zhangrui.concurrency;
+
+public class ReentrantDemo {
+    public synchronized void demo(){
+        System.out.println("begin:demo");
+        demo2();
+    }
+    public void demo2(){
+        System.out.
+            println("begin:demo1");
+        synchronized (this){}
+    }
+    public static void main(String[] args) {
+        ReentrantDemo rd=new ReentrantDemo();
+        new Thread(rd::demo).start();
+    }
+}
