5fb41391cd95efbbf1c5e24c62ac6624cd85d086
zhangrui <1037102215@qq.com>
2020-07-15 20:56:58 +0800

    update

---
+++ zhangrui-api/src/main/java/com/zhangrui/concurrency/atomic/IPlus.java
@@ -0,0 +1,15 @@
+package com.zhangrui.concurrency.atomic;
+
+/**
+ * @description: Volatile加
+ * @author: ZhangRui
+ * @create: 2020-07-07 11:23
+ **/
+public class IPlus {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println(i);
+    }
+
+}
