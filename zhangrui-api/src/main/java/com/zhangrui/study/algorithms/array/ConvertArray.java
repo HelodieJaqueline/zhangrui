07f3c213603eac49947540c0e4d0f9ad130f0af7
zhangrui <1037102215@qq.com>
2022-03-16 22:59:52 +0800

    convertArray

--- zhangrui-api/src/main/java/com/zhangrui/study/algorithms/array/ConvertArray.java
+++ zhangrui-api/src/main/java/com/zhangrui/study/algorithms/array/ConvertArray.java
@@ -9,21 +9,22 @@ import java.util.Arrays;
  */
 public class ConvertArray {
 
-    public static void convertArray(int[] nums){
-        int i = 0, j = nums.length - 1;
-        while (i < j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
+    public static int[] convertArray(int[] nums){
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
+        return nums;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4};
-        convertArray(nums);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = {7, 2, 8, 4, 10};
+        int[] convertArray = convertArray(nums);
+        System.out.println(Arrays.toString(convertArray));
 
     }
 
