2fafc1192dd730d86c8558682068644a2db41694
zhangrui <1037102215@qq.com>
2023-02-06 21:40:35 +0800

    removeElement

---
+++ zhangrui-api/src/main/java/com/zhangrui/study/algorithms/RemoveElement.java
@@ -0,0 +1,19 @@
+package com.zhangrui.study.algorithms;
+
+public class RemoveElement {
+
+    class Solution {
+        public int removeElement(int[] nums, int val) {
+            int fast = 0, slow = 0;
+            while (fast < nums.length) {
+                if (nums[fast] != val) {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+                fast++;
+            }
+            return slow;
+        }
+    }
+
+}
