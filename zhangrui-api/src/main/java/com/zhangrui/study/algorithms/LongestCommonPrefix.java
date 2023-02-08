4b39cbd43b9ce18116b85bce1c11fef6fb148e11
zhangrui <1037102215@qq.com>
2023-02-08 20:09:08 +0800

    longestCommonPrefix

---
+++ zhangrui-api/src/main/java/com/zhangrui/study/algorithms/LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+package com.zhangrui.study.algorithms;
+
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        int m = strs.length;
+        // 以第一行的列数为基准
+        int n = strs[0].length();
+        for (int col = 0; col < n; col++) {
+            for (int row = 1; row < m; row++) {
+                String thisStr = strs[row], prevStr = strs[row - 1];
+                // 判断每个字符串的 col 索引是否都相同
+                if (col >= thisStr.length() || col >= prevStr.length() ||
+                        thisStr.charAt(col) != prevStr.charAt(col)) {
+                    // 发现不匹配的字符，只有 strs[row][0..col-1] 是公共前缀
+                    return strs[row].substring(0, col);
+                }
+            }
+        }
+        return strs[0];
+    }
+
+}
