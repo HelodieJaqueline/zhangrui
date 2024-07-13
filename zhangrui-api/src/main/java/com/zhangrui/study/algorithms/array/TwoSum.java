4150bd0877d13716c4053702b65d7835fd7a3be9
zhangrui <1037102215@qq.com>
2024-07-13 22:20:50 +0800

    TwoSum

---
+++ zhangrui-api/src/main/java/com/zhangrui/study/algorithms/array/TwoSum.java
@@ -0,0 +1,21 @@
+package com.zhangrui.study.algorithms.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int result = target - nums[i];
+            if (map.containsKey(result)) {
+                return new int[]{i, map.get(result)};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+}
